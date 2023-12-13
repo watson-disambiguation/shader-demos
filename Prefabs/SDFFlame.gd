3851084f7bea6594589032d0987bfcaf6c0785b4
wwatson <wdfwatson@gmail.com>
2023-12-13 14:03:56 -0400

    Modified shader to have better parameters

--- Prefabs/SDFFlame.gd
+++ Prefabs/SDFFlame.gd
@@ -5,13 +5,11 @@ extends Node3D
 func _ready():
 	var mat: ShaderMaterial = mesh.get_active_material(0)
 	mat = mat.duplicate()
+	var old_noise_tex = mat.get_shader_parameter("noiseTex");
+	var old_noise = old_noise_tex.noise
 	mesh.set_surface_override_material(0,mat)
-	var noise_tex = NoiseTexture3D.new()
-	noise_tex.seamless = true
-	noise_tex.seamless_blend_skirt = 0.7
-	var noise = FastNoiseLite.new()
-	noise.noise_type = FastNoiseLite.TYPE_PERLIN
-	noise.frequency = 0.05
+	var noise_tex = old_noise_tex.duplicate()
+	var noise = old_noise.duplicate()
 	noise.seed = randi()
 	noise_tex.noise = noise
 	mat.set_shader_parameter("noiseTex",noise_tex)
