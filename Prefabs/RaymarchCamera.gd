116b200d8f7ef0c30d08f04b5509c83638f31441
Watson <wdfwatson@gmail.com>
2024-01-07 18:43:59 -0400

    Added Ambient Occlusion and Gooch-based shading

--- Prefabs/RaymarchCamera.gd
+++ Prefabs/RaymarchCamera.gd
@@ -6,13 +6,22 @@ class_name RaymarchCamera
 @export var rotation_speed: float
 @export var speed: float = 1
 @onready var quad = $MeshInstance3D
+var use_mouse = true;
 
+func set_use_mouse(val: bool):
+	use_mouse = val
+	if val:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_use_mouse(true)
 	quad.mesh.material = raymarch_shader
 
 func _process(delta):
+	print(Input.mouse_mode)
 	var theta = rotation.y
 	var vert = Vector3.UP * Input.get_axis("ui_in","ui_out")
 	var forward = Vector3(-sin(theta),0,-cos(theta)) * Input.get_axis("ui_down","ui_up")
@@ -24,7 +33,11 @@ func _process(delta):
 	print(Engine.get_frames_per_second())
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and use_mouse:
 		rotation.x += event.relative.y * rotation_speed
 		rotation.y += event.relative.x * rotation_speed
 		rotation.x = clamp(rotation.x, -PI/2, PI/2 )
+	if event.is_action_pressed("ui_cancel"):
+		set_use_mouse(!use_mouse)
+		
+	
