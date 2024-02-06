da8c5a123c5cd18a8e5bdd740fcdc97edb5111d5
Watson <wdfwatson@gmail.com>
2024-02-05 20:39:28 -0400

    Began work on skybox shader

--- Prefabs/CameraMover.gd
+++ Prefabs/CameraMover.gd
@@ -4,7 +4,6 @@ class_name CameraMover
 
 @export var rotation_speed: float
 @export var speed: float = 1
-@onready var quad = $MeshInstance3D
 var use_mouse = true;
 
 func set_use_mouse(val: bool):
@@ -19,10 +18,9 @@ func _ready():
 	set_use_mouse(true)
 
 func _process(delta):
-	print(Input.mouse_mode)
 	var theta = rotation.y
 	var vert = Vector3.UP * Input.get_axis("ui_in","ui_out")
-	var forward = Vector3(sin(theta),0,cos(theta)) * Input.get_axis("ui_down","ui_up")
+	var forward = Vector3(-sin(theta),0,-cos(theta)) * Input.get_axis("ui_down","ui_up")
 	var side = Vector3(cos(theta),0,-sin(theta)) * Input.get_axis("ui_left","ui_right")
 	var velocity = vert + forward + side
 	velocity = velocity.normalized() * delta * speed
@@ -30,8 +28,8 @@ func _process(delta):
 
 func _input(event):
 	if event is InputEventMouseMotion and use_mouse:
-		rotation.x += event.relative.y * rotation_speed
-		rotation.y += event.relative.x * rotation_speed
+		rotation.x -= event.relative.y * rotation_speed
+		rotation.y -= event.relative.x * rotation_speed
 		rotation.x = clamp(rotation.x, -PI/2, PI/2 )
 	if event.is_action_pressed("ui_cancel"):
 		set_use_mouse(!use_mouse)
